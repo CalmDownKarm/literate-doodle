5e1c52b6d8630d1e44bf6f6f49d01d7458586013
calmdownkarm <karmanyaaggarwal@gmail.com>
2020-02-22 15:24:06 +0530

    updates

--- js/marimeko.js
+++ js/marimeko.js
@@ -1,23 +1,23 @@
 var marimekkoSpec = {
   "$schema": "https://vega.github.io/schema/vega/v5.json",
   "description": "todo",
-  "width": 250,
+  "width": 390,
   "height": 250,
   "padding": 5,
-  "autosize": { "type": "fit", "contains": "padding" },
+  "autosize": {"type": "fit", "contains": "padding"},
   "data": [
     {
       "name": "table",
-      "values": {},
+
       "transform": [
-        { "type": "nest", "keys": ["Region", "Year"] },
+        {"type": "nest", "keys": ["Department", "Status"]},
         {
           "type": "treemap",
-          "field": "Sales",
+          "field": "Status|count",
           "method": "slicedice",
           "ratio": 1,
           "paddingInner": 1,
-          "size": [{ "signal": "height" }, { "signal": "width" }],
+          "size": [{"signal": "height"}, {"signal": "width"}],
           "as": ["y0", "x0", "y1", "x1", "depth", "children"]
         }
       ]
@@ -25,41 +25,43 @@ var marimekkoSpec = {
     {
       "name": "labels",
       "source": "table",
-      "transform": [{ "type": "filter", "expr": "datum.x0 == 0" }]
+      "transform": [{"type": "filter", "expr": "datum.x0 == 0"}]
     }
   ],
   "scales": [
     {
       "name": "color",
       "type": "ordinal",
-      "domain": { "data": "table", "field": "Year" },
-      "range": ["#96cbff", "#1A8CFF", "#115DAA", "#082A4D"]
+      "domain": {"data": "table", "field": "Status"},
+      "range": ["#1e7e34","#0062cc","#dc3545"]
     }
   ],
   "marks": [
     {
       "type": "rect",
-      "from": { "data": "table" },
+      "from": {"data": "table"},
       "encode": {
         "update": {
-          "x": { "field": "x0" },
-          "y": { "field": "y0" },
-          "x2": { "field": "x1" },
-          "y2": { "field": "y1" },
-          "fill": { "scale": "color", "field": "Year" },
-          "tooltip": { "signal": "datum" }
+          "x": {"field": "x0"},
+          "y": {"field": "y0"},
+          "x2": {"field": "x1"},
+          "y2": {"field": "y1"},
+          "fill": {"scale": "color", "field": "Status"},
+          "tooltip": {"signal": "datum"}
         }
       }
     },
     {
       "type": "text",
-      "from": { "data": "labels" },
+      "from": {"data": "labels"},
       "encode": {
         "update": {
-          "y": { "signal": "(datum.y0 + datum.y1)/2" },
-          "x": { "value": -5 },
-          "text": { "field": "Region" },
-          "align": { "value": "right" }
+          "y": {"signal": "(datum.y0 + datum.y1)/2"},
+          // "x": {"value": -5},
+          "text": {"field": "Department"},
+          "align": {"value": "top"},
+          // "angle":{"value": -45},
+          "fontStyle":{"value":"italic"}
         }
       }
     }
