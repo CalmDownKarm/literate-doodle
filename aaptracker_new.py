abce141fd67d694ce11b38c8f8ebdc3eae5d0218
calmdownkarm <karmanyaaggarwal@gmail.com>
2020-03-03 15:19:42 +0530

    pushing updates file

--- aaptracker_new.py
+++ aaptracker_new.py
@@ -1,11 +1,11 @@
 import pandas as pd
-ideal_promises = pd.read_excel('../assets/data/Promises.xlsx')
+ideal_promises = pd.read_excel('assets/data/Promises.xlsx')
 ideal_promises.to_excel('newPromises.xlsx', index=None)
 ideal_promises.Promise = ideal_promises.Promise.apply(lambda x: x.lower().strip())
 ideal_promises['Sub promise'] = ideal_promises['Sub promise'].apply(lambda x: x.lower().strip())
 
 # point this to where the new reponses will be donwloaded
-updates = pd.read_excel('../assets/data/newResponses.xlsx')
+updates = pd.read_excel('assets/data/newResponses.xlsx')
 
 
 updates.Promise = updates.Promise.apply(lambda x: x.lower().strip())
@@ -27,11 +27,11 @@ updates = updates.drop(subpromise_error_indices)
 updates.to_excel('newupdates.xlsx')
 
 # read current updates
-old_updates = pd.read_excel('../assets/data/updates.xlsx')
+old_updates = pd.read_excel('assets/data/updates.xlsx')
 
 old_updates['SubPromise'] = old_updates['SubPromise'].apply(lambda x: x.split('__')[-1].lower().strip())
 # concat updates
 old_and_new_updates = pd.concat([updates, old_updates])
 merged_df = pd.merge(old_and_new_updates, ideal_promises, left_on='SubPromise', right_on = 'Sub promise')
 #write to file
-merged_df.to_excel('../assets/data/updates.xlsx', index=None)
+merged_df.to_excel('assets/data/updates.xlsx', index=None)
