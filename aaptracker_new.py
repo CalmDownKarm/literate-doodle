456bf1de5ee841276dfbb02d2075b5944684bda0
calmdownkarm <karmanyaaggarwal@gmail.com>
2020-03-20 21:35:53 +0530

    fixed backend bug

--- aaptracker_new.py
+++ aaptracker_new.py
@@ -1,43 +1,33 @@
-def schedule_me():
-    import pandas as pd
+import re
+import requests
+import pandas as pd
+from fuzzywuzzy import process
 
-    # clean new updates
-    ideal_promises = pd.read_excel('./assets/data/Promises.xlsx')
-    # ideal_promises.to_excel('newPromises.xlsx', index=None)
-    ideal_promises.Promise = ideal_promises.Promise.apply(lambda x: x.lower().strip())
-    ideal_promises['Sub promise'] = ideal_promises['Sub promise'].apply(lambda x: x.lower().strip())
-    import requests
+
+def schedule_me():
+    ideal_promises = pd.read_excel('assets/data/Promises.xlsx')
     url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQi5APAZW3CvMw-8LUdCFxdg5CIEjAB-EnnKHe6kzMxQyHDWZbZToW3kLSvpDMcEQZ34o6nOIxbMcwm/pub?output=xlsx"
     googleFormUpdates = requests.get(url)
-    open('./assets/data/googleFormUpdates.xlsx', 'wb').write(googleFormUpdates.content)
-    updates = pd.read_excel('./assets/data/googleFormUpdates.xlsx')
-    updates = updates.rename(columns={'Promise ': 'Promise', 'SubPromise ': 'SubPromise'})
-    updates['Promise'].fillna('None', inplace=True)
-    updates['Promise'] = updates['Promise'].apply(lambda x: x.lower().strip())
-    correct_subpromises = ideal_promises['Sub promise'].unique()
-    correct_promises = set([promise.lower() for promise in ideal_promises.Promise.unique()])
-    assert len(correct_promises) == 70
-    updates_promises = set([promise.lower() for promise in updates['Promise'].unique()])
+    with open('assets/data/googleFormUpdates.xlsx', 'wb') as f:
+        f.write(googleFormUpdates.content)
+    updates = pd.read_excel('assets/data/googleFormUpdates.xlsx').rename(columns={'Promise ': 'Promise', 'SubPromise ': 'Sub promise'}).dropna(how='all')
+    for col in ['Promise', 'Sub promise']:
+        ideal_promises[col] = ideal_promises[col].apply(lambda x: x.lower().strip())
+        updates[col] = updates[col].apply(lambda x: x.lower().strip())
+    correct_subpromises = set(ideal_promises['Sub promise'].unique())
+    correct_promises = set(ideal_promises['Promise'].unique())
+    updates_promises = set(updates['Promise'].unique())
     errors = updates_promises - correct_promises
-    from fuzzywuzzy import process
     corrections = {k:process.extract(k, correct_promises, limit=1)[0][0] for k in errors}
     maps = {k:k for k in updates['Promise'].unique()}
     maps.update(corrections)
     updates['Promise'] = updates['Promise'].map(maps)
-    updates['SubPromise'].fillna('None', inplace=True)
-    updates['SubPromise'] = updates['SubPromise'].apply(lambda x: x.split('__')[-1].lower().strip())
-    subpromise_errors = set(updates['SubPromise'].unique()) - set(correct_subpromises)
-    corrections = {k:process.extract(k, correct_subpromises, limit=2) for k in subpromise_errors}
-    subpromise_error_indices = updates[updates['SubPromise'].isin(subpromise_errors)].index
-    updates = updates.drop(subpromise_error_indices)
-    updates.to_excel('./assets/data/googleFormUpdates.xlsx')
+    updates['Sub promise'] = updates['Sub promise'].apply(lambda x: x.split('__')[-1].lower().strip())
+    updates[updates['Sub promise'].isin(correct_subpromises)].to_excel('assets/data/googleFormUpdates.xlsx', index=None)
+    oldupdates = pd.read_excel('assets/data/old_website_updates.xlsx')
+    combined = pd.concat([updates, oldupdates], sort=False)
+    merged_df = pd.merge(combined, ideal_promises, left_on='Sub promise', right_on = 'Sub promise')
+    merged_df = merged_df.drop(['Department_x', 'Promise_x', 'Outcome_x', 'Sr No'], axis=1).rename(columns={'Promise_y':'Promise', 'Department_y':'Department', 'Outcome_y':'Outcome'})
+    merged_df['Department'] = merged_df['Department'].apply(lambda x: re.sub('\sDepartment', '', x))
+    merged_df.to_excel('assets/data/updates.xlsx', index=None)
 
-    # Merge old and new updates
-    oldupdates = pd.read_excel('./assets/data/old/updates.xlsx')
-    oldupdates['Promise'] = oldupdates['Promise'].apply(lambda x: x.lower().strip())
-    old_and_new_updates = pd.concat([updates, oldupdates], sort=False)
-    merged_df = pd.merge(old_and_new_updates, ideal_promises, left_on='SubPromise', right_on = 'Sub promise')
-    merged_df.drop(merged_df.columns[merged_df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
-    merged_df.drop(['Promise_x', '__', 'Sr No'],axis=1, inplace=True)
-    merged_df = merged_df.rename(columns={'Promise_y': 'Promise'})
-    merged_df.to_excel('./assets/data/updates.xlsx')
